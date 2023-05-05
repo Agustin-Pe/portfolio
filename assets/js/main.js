7e8a0748b9770b85b06bff1d27e0c97f878a92cd
Agustin-Pe <112834687+Agustin-Pe@users.noreply.github.com>
2023-05-04 23:04:28 -0300

    Se finalizaron todas las secciones y está en procedimiento el scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,17 +44,72 @@ let swiperProjects = new Swiper(".projects__container", {
     },
   });
 
-
-
-
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+
+  if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+     contactMessage.classList.remove('color-blue')
+     contactMessage.classList.add('color-red')
+
+     contactMessage.textContent = 'Write all the input fields'
+  }else{
+    emailjs.sendForm('service_7ge3qcs','template_prn9qr7','#contact-form','JXBs6EmatR7JFQiY9')
+        .then(() =>{
+          contactMessage.classList.add('color-blue')
+          contactMessage.textContent = 'Mensaje enviado ✅'
+
+          setTimeout(() =>{
+            contactMessage.textContent = ''
+          }, 5000)
+        },(error) =>{
+          alert('No se ha podido enviar el mensaje', error)
+        })
+
+        contactName.value = ''
+        contactEmail.value = ''
+        contactProject.value = ''
+  }
+
+}
+contactForm.addEventListener('submit', sendEmail)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
